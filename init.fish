2acfac053e534c00267ba40f752b516f4d68aba2
Walter Dolce <walterdolce@gmail.com>
2017-05-05 17:28:55 +0100

    Automatically assign a TTY to the GPG_TTY var

--- init.fish
+++ init.fish
@@ -1,6 +1,7 @@
 
 set -x SVN_EDITOR vi
 set -xg EDITOR vi
+set -x GPG_TTY (tty)
 
 set -g theme_avoid_ambiguous_glyphs yes
 set -g theme_display_cmd_duration yes
@@ -14,6 +15,7 @@ set -g theme_display_vi yes
 set -g theme_display_virtualenv yes
 set -g theme_title_display_process yes
 set -g theme_title_use_abbreviated_path yes
+set -g theme_nerd_fonts yes
 
 for i in /usr/local/sbin $HOME/.rbenv/shims
     if not contains $i $PATH
