966e48e5910ab204d3e22af26b31499187edb2d5
Walter Dolce <walterdolce@gmail.com>
2018-02-28 17:04:51 +0000

    Add output information to the script

--- provision.sh
+++ provision.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+echo "Starting provisioning of 'my-omf-config'..."
+echo "Current working directory is: ${PWD}"
+echo $(realpath $0)
 if test -z "${OMF_CONFIG}"; then
     echo "Could not provision oh-my-fish config to \$OMG_CONFIG as it's not set."
     exit 1
@@ -8,3 +11,6 @@ fi
 cp ./bundle ${OMF_CONFIG}
 cp ./init.fish ${OMF_CONFIG}
 cp ./theme ${OMF_CONFIG}
+
+echo "Done."
+
